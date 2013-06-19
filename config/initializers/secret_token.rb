fa3b727e7e93ee3685ac0ab947ff0d75809174eb
Julio Lucero <poramo@gmail.com>
2013-06-19 16:12:27 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AltorosZombies::Application.config.secret_key_base = '353d7da2ae13b746e124e4d952c485f79dff0a72a6aa3bf21088c5d00a3e6c987de71aaf0797a70dcd5654eb46b1e7a5e3d6ce8b48207f621b15db5ae4f731bf'
